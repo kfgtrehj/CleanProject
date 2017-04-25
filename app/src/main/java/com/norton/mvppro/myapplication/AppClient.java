27c7e66b125760522b3d08a1edc1f9e83715cacd
huzhenzhen <396409559@qq.com>
2017-04-25 15:17:24 +0800

    init project

--- app/src/main/java/com/norton/mvppro/myapplication/AppClient.java
+++ app/src/main/java/com/norton/mvppro/myapplication/AppClient.java
@@ -1,10 +1,6 @@
 package com.norton.mvppro.myapplication;
 
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-
 import io.reactivex.Observable;
-import retrofit2.GsonConverterFactory;
-import retrofit2.Retrofit;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -18,17 +14,6 @@ import retrofit2.http.Path;
  */
 
 public class AppClient {
-    static Retrofit mRetrofit;
-    public static Retrofit retrofit() {
-        if (mRetrofit == null) {
-            mRetrofit = new Retrofit.Builder()
-                    .baseUrl("http://www.weather.com.cn/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .build();
-        }
-        return mRetrofit;
-    }
     public interface ApiStores {
         @GET("adat/sk/{cityId}.html")
         Observable<WeatherJson> getWeather(@Path("cityId") String cityId);
